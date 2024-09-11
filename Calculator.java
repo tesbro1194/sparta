0823a94b601f6ce2b6d4a7710eb2b2ede69958a8
tesbro1194 <tes1194@naver.com>
2024-09-11 14:53:33 +0900

    exitCalculator 메서드 추가, getResultsList 수정, showResultsList 삭제

--- Calculator.java
+++ Calculator.java
@@ -1,11 +1,13 @@
 package calculator.CalculatorLv2;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class Calculator {
     private final static ArrayList<Integer> resultsList = new ArrayList<>();
     static int number;
+    static boolean exitKey;
 
     static int addCalculate (int firstNumber, int secondNumber) {
         number = firstNumber + secondNumber;
@@ -30,8 +32,7 @@ public class Calculator {
         return number;
     }
 
-    static ArrayList<Integer> getResultsList (int number) {
-        resultsList.add(number);
+    static ArrayList<Integer> getResultsList () {
         return resultsList;
     }
 
@@ -41,14 +42,23 @@ public class Calculator {
         return resultsList;
     }
 
-    static ArrayList<Integer> showResultsList (){
-        resultsList.toString();
-        return resultsList;
-    }
 
     static ArrayList<Integer> removeFirstResult () {
         resultsList.remove(resultsList.getFirst());
         System.out.println(resultsList);
         return resultsList;
+    }
+
+    static boolean exitCalculator () {
+        System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+        Scanner inputExit = new Scanner(System.in);
+        String exitText = inputExit.next();
+        if ("exit".equals(exitText)){
+            exitKey=false;
+        }
+        else {
+            exitKey=true;
+        }
+        return exitKey;
     }
 }
